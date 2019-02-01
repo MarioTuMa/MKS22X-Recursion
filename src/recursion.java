34ca14a2fd76b11cf6dbd71fd6cf8b3bc5cd12d2
exnester <mtutuncumacias1@gmail.com>
2019-01-31 20:28:31 -0500

    Finished class by adding makeAllSums (may be a bit less efficient than possible as there is a loop in the inside of the recursion)

--- src/recursion.java
+++ src/recursion.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class recursion {
 
@@ -31,11 +32,33 @@ public class recursion {
 		return fibH(n,0,1);
     }
 	
+	public static ArrayList<Integer> makeAllSums(int n){
+		if(n == 1) {
+			ArrayList<Integer> newList = new ArrayList<Integer>();
+			newList.add(0);
+			newList.add(1);
+			return newList;
+		}
+		ArrayList<Integer> newList = new ArrayList<Integer>();
+		ArrayList<Integer> halfList = new ArrayList<Integer>();
+		halfList = makeAllSums(n - 1);
+		ArrayList<Integer> otherHalfList = new ArrayList<Integer>();
+		for(int i = 0; i < halfList.size(); i ++) {
+			otherHalfList.add(halfList.get(i) + n);
+		}
+		newList.addAll(halfList);
+		newList.addAll(otherHalfList);
+
+		return newList;
+	
+	}
+	
 	public static void main(String [] args)
 	{
 		System.out.println(sqrt(5,0.001));
 		System.out.println(sqrt(4,0.0000000001));
 		System.out.println(fib(4000));
+		System.out.println(makeAllSums(10));
 	}
 
 }
