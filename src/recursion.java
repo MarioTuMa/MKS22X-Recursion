2688b7919910f7b2f3d1cf9df6e61eb40eb8cd81
exnester <mtutuncumacias1@gmail.com>
2019-01-31 20:16:36 -0500

    Added fibonacci method
    Fibonacci’s start 1,1,2,3 and is 1 indexed like they should be

--- src/recursion.java
+++ src/recursion.java
@@ -18,10 +18,24 @@ public class recursion {
 	public static double sqrt(double n, double tolerance){
 		return sqrtH(n,1,tolerance);
 	}
+	
+	public static int fibH(int n, int f_1, int f_2){
+		if(n == 0) {
+			return f_1;
+		}
+		else {
+			return fibH( n - 1, f_1 + f_2, f_1);
+		}
+    }
+	public static int fib(int n){
+		return fibH(n,0,1);
+    }
+	
 	public static void main(String [] args)
 	{
 		System.out.println(sqrt(5,0.001));
 		System.out.println(sqrt(4,0.0000000001));
+		System.out.println(fib(4000));
 	}
 
 }
